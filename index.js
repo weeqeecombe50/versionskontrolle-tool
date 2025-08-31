94790f0250d3c38ec462ee8c0459001cc7faa187
weeqeecombe50 <mynieagbona@hotmail.com>
2025-08-31 18:04:21 +0300

    Update help message to be more descriptive

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { startCLI, startGUI } = require('./vkt');
 const args = process.argv.slice(2);
 
 if (args.includes('--help')) {
-    console.log('Kommandozeilen-Hilfe');
+    console.log('Kommandozeilen-Hilfe: Verwendung: node index.js [gui | --help]');
 } else if (args.includes('gui')) {
     startGUI();
 } else {
