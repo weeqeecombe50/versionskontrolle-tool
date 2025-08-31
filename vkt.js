6341e40ef0561eb5c1d06317712d8e7db9298df1
weeqeecombe50 <mynieagbona@hotmail.com>
2025-08-31 18:01:09 +0300

    feat: initial commit for vkt.js

---
+++ vkt.js
@@ -0,0 +1,13 @@
+// Die Hauptlogik der Versionskontrolle
+
+function startCLI() {
+    console.log('CLI gestartet...');
+    // Logik für die Kommandozeilenoberfläche
+}
+
+function startGUI() {
+    console.log('GUI gestartet...');
+    // Logik für die grafische Benutzeroberfläche
+}
+
+module.exports = { startCLI, startGUI };
