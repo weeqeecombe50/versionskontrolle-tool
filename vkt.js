98e7a893721ff79f6d5f76b9ca4f89a39c0b7a3d
weeqeecombe50 <mynieagbona@hotmail.com>
2025-08-31 18:04:09 +0300

    Refactor the startCLI and startGUI functions for better logging

--- vkt.js
+++ vkt.js
@@ -1,12 +1,12 @@
 // The main logic of version control
 
 function startCLI() {
-    console.log('CLI started...');
+    console.log('[INFO] CLI started...');
     // Logic for command-line interface
 }
 
 function startGUI() {
-    console.log('GUI started...');
+    console.log('[INFO] GUI started...');
     // Logic for graphical user interface
 }
 
